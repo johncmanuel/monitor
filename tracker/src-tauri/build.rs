131e9e3908df05a38ed0d441e9b67fe2cb847835
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-08-23 11:07:45 -0700

    modify build script to account for environment

--- tracker/src-tauri/build.rs
+++ tracker/src-tauri/build.rs
@@ -1,10 +1,17 @@
 use std::env;
 
 fn main() {
-    dotenvy::dotenv().ok();
+    let profile = env::var("PROFILE").unwrap_or_else(|_| "dev".into());
 
-    println!("cargo:rerun-if-changed=.env");
+    if profile == "release" {
+        dotenvy::from_filename(".env.production").ok();
+        println!("cargo:rerun-if-changed=.env.production");
+    } else {
+        dotenvy::from_filename(".env").ok();
+        println!("cargo:rerun-if-changed=.env");
+    }
 
+    // Pass all env vars to rustc
     for (key, value) in env::vars() {
         println!("cargo:rustc-env={}={}", key, value);
     }
