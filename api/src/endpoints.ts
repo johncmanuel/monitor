5ea54347b5ad7fd4ab679a5e24e7ad4232e1f310
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-08-23 09:17:25 -0700

    logging for endpoints

--- api/src/endpoints.ts
+++ api/src/endpoints.ts
@@ -9,13 +9,18 @@ export const trackerHandler: EndpointHandler = async (request) => {
       request.headers.get("Authorization") !==
         `Bearer ${Deno.env.get("API_KEY")!}`
     ) {
+      console.log("Unauthorized request");
       return new Response("Unauthorized", { status: 401 });
     }
     const data: Data = await request.json();
+    console.log(`Received data at timestamp ${data.ts}`);
     await storeTrackerData(data);
+    console.log(`successfully stored data for ${data.ts}`);
     return new Response("Data received", { status: 200 });
   } else if (request.method === "GET") {
+    console.log("Fetching data");
     const data = await getTrackerData();
+    console.log("Fetched data");
     return new Response(JSON.stringify(data), { status: 200 });
   }
   return new Response("Method not allowed", { status: 405 });
